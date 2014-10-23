113012ca49abc457ed48e8fcd4fb6a4af084bf86
reeonce <reeonce@yahoo.com>
2014-10-23 12:37:50 +0800

    update about page

--- about.md
+++ about.md
@@ -2,6 +2,7 @@
 layout: page
 title: About
 permalink: /about/
+css: "/css/about.css"
 ---
 
 ### Introduction
@@ -16,27 +17,62 @@ Now I prefer to working with the exciting area - **iOS** development.
 
 ### My Career
 
-<div class="timeline">
-	<svg>
-	</svg>
+<div id="timeline" class="slideshow">
+  <div id="career-timelime" class="slideshow-item">
+  	<svg>
+  	</svg>
+  </div>
+  <div id="career-buaa" class="slideshow-item">
+    <div class="content">
+      <h3>@Beihang University</h3>
+      <ul>
+        <li>I got my bachelor's degree on software engineering in Beihang University in Jun 2013.</li>
+        <li>During April 2011 to October 2011, I joined in my school's ACM Programming Design Training, and got several prizes.</li>
+        <li>In April 2012, I got the second prize in the Twenty-Second Feng Ru Cup technology competition.</li>
+        <li>During May 2012 to October 2012, I had an internship in Microsoft STC for five months.</li>
+      </ul>
+    </div>
+  </div>
+  <div id="career-yahoo" class="slideshow-item">
+    <div class="content">
+      <h3>@Yahoo</h3>
+      <p>As a freshman among so many talents at Yahoo, my main work is learning techonical and non-techonical skills from them. More than one years' time, I worked in the scrum team with teammates, learnt development and CI on the backend, dealt with frontend issues.</p>
+    </div>
+  </div>
+  <div id="career-thinking" class="slideshow-item">
+    <div class="content">
+      <h3>@where?</h3>
+      <p>During these days, I haven't looking for my next job immediately.I need think about it, so most of my time was spent on reading and learning. </p>
+      <p>The most important, I leart iOS development. And publish two apps, named "Password Securer" and "Trap the Dot !"<p>
+    </div>
+  </div>
 </div>
-<style type="text/css">
-   svg {
-      font-size: 10px;
-    }
-    g {
-      overflow: visible;
-    }
-    text {
-      z-index: 20;
-    }
-    rect.pane {
-      cursor: move;
-      fill: none;
-      pointer-events: all;
-    }
-</style>
-<script type="text/javascript" src="http://d3js.org/d3.v3.min.js"></script>
-<script type="text/javascript" src="/js/d3-timeline.js"></script>
+
+### Major Skills
+#### iOS Engineer
+  two apps:
+
+  **Passwords Securer**
+
+  **Trap The Dot**
+
+#### Frontend Engineer
+  Familiar with html, css and Javascript.
+
+  **mobile.yahoo.com**
+
+#### Backend Engineer
+  Had been using Node.js for years;
+
+  **CloudKV**
+
+<div id="welcome-overlay">
+  <h1>Reeonce</h1>
+  <p id="welcome-alert" style="display: none">Please make sure your browser is HTML5 supported.<p>
+</div>
+<script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+<script type="text/javascript" src="/js/d3.v3.min.js"></script>
+<script type="text/javascript" src="/js/modernizr.min.js"></script>
 <script type="text/javascript" src="/js/others/about.js"></script>
+
 
--- js/others/about.js
+++ js/others/about.js
@@ -1,14 +1,186 @@
-var testData = [
-	{label: "person a", times: [
-	{"starting_time": 1355752800000, "ending_time": 1355759900000},
-	{"starting_time": 1355767900000, "ending_time": 1355774400000}]},
-	{label: "person b", times: [
-	{"starting_time": 1355759910000, "ending_time": 1355761900000}]},
-	{label: "person c", times: [
-	{"starting_time": 1355761910000, "ending_time": 1355763910000}]},
+var addLines = function(group, linesData) {
+	var lines = group.data(linesData);
+	if (group[0].length == 0) {
+		lines = lines.enter()
+			.append("line");
+	}
+	lines.attr("x1", function (d) { return d.x1; })
+		.attr("y1", function (d) { return d.y1; })
+		.attr("x2", function (d) { return d.x2; })
+		.attr("y2", function (d) { return d.y2; })
+		.attr("class", function (d) { return d.class; })
+		.attr("stroke", "#222")
+		.attr("stroke-width", 3);
+}
+
+var addCircles = function(group, circlesData) {
+	var circles = group.data(spaceCircles);
+	if (group[0].length == 0) {
+		circles = circles.enter()
+			.append("circle");
+	}
+
+	circles.attr("cx", function (d) { return d; })
+		.attr("cy", 120)
+		.attr("r", 10 )
+		.style("fill", function(d) {
+			var returnColor;
+			if (d === 30) { returnColor = "green";
+			} else if (d === 70) { returnColor = "purple";
+			} else if (d === 110) { returnColor = "red"; }
+			return returnColor;
+		});
+}
+
+var addTexts = function(group, textsData) {
+	var texts = group.data(textsData);
+	if (group[0].length == 0) {
+		texts = texts.enter()
+			.append("text");
+	}
+	texts.attr("class", function (d) { return d.class; })
+		.attr("x", function (d) { return d.x; })
+		.attr("y", function (d) { return d.y; })
+		.attr("fill", "#222")
+		.attr("font-size", function (d) { return d["font-size"] || "18"; })
+		.attr("font-family", function (d) { return d["font-family"]; })
+		.text(function(d){ return d.text; });
+}
+
+var addLinks = function(group, textsData) {
+	var links = group.data(textsData);
+	if (group[0].length == 0) {
+		links = links.enter()
+			.append("a");
+	}
+	 links.attr("name", function (d) { return d.name; })
+	 	.attr("xlink:href", function (d) { return "#" + d.name; })
+	 	.attr("class", function (d) { return d.class; });
+	 var texts = links.select("text");
+	 if (!texts[0] || !texts[0][0]) {
+	 	texts = links.append("text");
+	}
+	texts.attr("x", function (d) { return d.x; })
+		.attr("y", function (d) { return d.y; })
+		.attr("fill", "#222")
+		.attr("font-size", function (d) { return d["font-size"] || "18"; })
+		.attr("font-family", function (d) { return d["font-family"]; })
+		.text(function(d){ return d.text; });
+}
+
+var totalWidth = $("#timeline").width();
+var svgWidth = totalWidth - 60;
+var svg = d3.select("#timeline svg");
+	
+var nowMonth = new Date();
+nowMonth.setHours(0,0,0,0);
+nowMonth.setDate(0);
+
+var dateFormat = d3.time.format("%Y-%m");
+var totalDuration = {
+	start_time: dateFormat.parse("2009-09"),
+	end_time: nowMonth
+}
+totalDuration.interval = totalDuration.end_time.getTime() - totalDuration.start_time.getTime();
+
+var durations = [
+	{start_time: dateFormat.parse("2009-09"), end_time: dateFormat.parse("2013-06"), text: "Beihang University", name: "buaa"},
+	{start_time: dateFormat.parse("2013-06"), end_time: dateFormat.parse("2014-07"), text: "YAHOO Beijing R&D", name: "yahoo"},
+	{start_time: dateFormat.parse("2014-07"), end_time: nowMonth, text: "Thinking & Learning ...", name: "thinking"}
 ];
-var w = 600,
-h = 500;
-var svg = d3.select(".timeline svg").attr("width", w);
-var chart = d3.timeline();
-svg.datum(testData).call(chart);
+
+var linesData = [];
+var spaceCircles = [];
+var textsData = [];
+var dateTextsData = [];
+var verticalLinesData = [];
+
+var getSvdData = function() {
+	$.each(durations, function(index, value) {
+		linesData[index] = {};
+		linesData[index].x1 = 44 + (value.start_time.getTime() - totalDuration.start_time.getTime()) / totalDuration.interval * svgWidth;
+		linesData[index].y1 = 120;
+		linesData[index].x2 = 20 + (value.end_time.getTime() - totalDuration.start_time.getTime()) / totalDuration.interval * svgWidth;
+		linesData[index].y2 = 120;
+		linesData[index].class = "line1";
+
+		spaceCircles[index] = linesData[index].x1 - 12;
+
+		dateTextsData[index] = {};
+		dateTextsData[index].text = value.start_time.getFullYear();
+		dateTextsData[index].x = spaceCircles[(index)]  - 20;
+		dateTextsData[index].y = 100;
+		dateTextsData[index].class = "text1";
+
+
+		verticalLinesData[index] = {};
+		verticalLinesData[index].x1 =  spaceCircles[index];
+		verticalLinesData[index].y1 =  132;
+		verticalLinesData[index].x2 =  spaceCircles[index];
+		verticalLinesData[index].y2 =  index * 60 + 200;
+		verticalLinesData[index].class = "line2";
+
+		textsData[index] = {};
+		textsData[index].text = value.text;
+		textsData[index].x = spaceCircles[index] - 20;
+		textsData[index].y = verticalLinesData[index].y2 + 20;
+		textsData[index].name = value.name;
+		textsData[index].class = "career-link";
+	});
+}
+var updateNodes = function() {
+	svg.attr("viewBox", "0 0 " + totalWidth + " 400");
+	addLines(svg.selectAll(".line1"), linesData);
+	addLines(svg.selectAll(".line2"), verticalLinesData);
+	addCircles(svg.selectAll("circle"), spaceCircles);
+	addTexts(svg.selectAll(".text1"), dateTextsData);
+	textsData[1].x = textsData[1].x - 60;
+	textsData[2].x = textsData[2].x - 120;
+	addLinks(svg.selectAll(".career-link"), textsData);
+}
+
+getSvdData();
+updateNodes();
+
+d3.selectAll(".career-link").on('click', function(event) {
+	d3.event.preventDefault();
+	$("#career-" + event.name).css("animation-name", "slideInFromRight");
+	$("#career-" + event.name).css("-webkit-animation-name", "slideInFromRight");
+	$("#career-" + event.name).css("left", "0");
+	$("#career-" + event.name).addClass("activate");
+	$("#career-timelime").css("animation-name", "slideOutToLeft");
+	$("#career-timelime").css("-webkit-animation-name", "slideOutToLeft");
+	$("#career-timelime").css("left", "-1000px");
+
+
+	$(".slideshow-item.activate").on('click', function(event) {
+		$(this).css("-webkit-animation-name", "slideOutToRight");
+		$(this).css("animation-name", "slideOutToRight");
+		$(this).css("left", "1000px");
+		$(this).removeClass("activate");
+		$("#career-timelime").css("animation-name", "slideOutToRight");
+		$("#career-timelime").css("-webkit-animation-name", "slideInFromLeft");
+		$("#career-timelime").css("left", "0px");
+	})
+})
+
+
+$(window).on('resize', function(event) {
+	totalWidth = $("#timeline").width();
+	svgWidth = totalWidth - 60;
+	getSvdData();
+	updateNodes();
+});
+
+if (Modernizr.cssanimations && Modernizr.svg) {
+	setTimeout(function() {
+		$("#welcome-overlay").css("animation-name", "fadeOut");
+		$("#welcome-overlay").css("-webkit-animation-name", "fadeOut");
+		setTimeout(function() {
+			$("#welcome-overlay").hide();
+			$("body").css("overflow", "auto");
+		}, 980);
+	}, 1000);
+} else {
+	$("#welcome-alert").show();
+}
